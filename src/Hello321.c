16935e44e12bc5ab0105a0fc8c8f4a5e6e6563f9
brent.horine <brent.horine@manhattan.edu>
2017-09-05 11:16:29 -0400

    Added Hello321 source files

---
+++ src/Hello321.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Hello321.c
+ Author      : Brent Horine
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello Embedded Systems!!!"); /* prints !!!Hello Embedded Systems!!! */
+	return EXIT_SUCCESS;
+}
